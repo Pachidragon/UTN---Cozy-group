06324c185d0bac82e4f3c455543d968d8dd7f135
TuliLuv <violeop1447@gmail.com>
2026-09-13 12:57:43 -0300

    Rotación implementada

--- proyecto en unity/Assets/Scripts/MovementSystem/PlayerMovement.cs
+++ proyecto en unity/Assets/Scripts/MovementSystem/PlayerMovement.cs
@@ -3,12 +3,14 @@ using UnityEngine.InputSystem;
 
 
 [RequireComponent(typeof(CharacterController))]
-public class NewMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Vector2 input;
     private CharacterController characterController;
     private Vector3 direction;
 
+    [SerializeField] private float smoothTime;
+    private float currentVelocity;
     [SerializeField] private float speed;
 
     private void Awake()
@@ -18,6 +20,12 @@ public class NewMovement : MonoBehaviour
 
     private void Update()
     {
+        if (input.sqrMagnitude == 0) return;
+        
+        var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
         characterController.Move(direction * speed * Time.deltaTime);
     }
 
