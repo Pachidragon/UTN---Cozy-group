8958752d3968d95d0043232afefeb7dd51d15d4f
bdb94 <heartless-94@hotmail.com.ar>
2026-09-14 17:16:58 -0300

    Layer interactable items

--- proyecto en unity/Assets/Scripts/Interaction System/Components/PlayerInteractor.cs
+++ proyecto en unity/Assets/Scripts/Interaction System/Components/PlayerInteractor.cs
@@ -28,13 +28,15 @@ public class PlayerInteractor : MonoBehaviour
 
     private void PlayerInteracted(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if (context.started)
         {
             Ray ray = new Ray(interactPivot.position, interactPivot.forward);
 
-            if (!Physics.Raycast(ray, out RaycastHit hitInfo, interactionDistance)) return;
+            int capaInteractuable = LayerMask.GetMask("Interactuable");
 
-            if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactableObject))
+            if (!Physics.Raycast(ray, out RaycastHit hitInfo, interactionDistance, capaInteractuable)) return;
+
+            if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactableObject))
             {
                 interactableObject.Interact();
             }
